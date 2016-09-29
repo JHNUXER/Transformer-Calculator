9d155abdf912f07c28ef74e0a3e365f8a960aedc
John Kayne <johnhoganuke2@gmail.com>
2016-09-29 10:58:20 -0400

    Update Transformer.js

--- Transformer.js
+++ Transformer.js
@@ -1,3 +1,12 @@
 class Transformer {
-
+  var inputAmperage;
+  var outputAmperage;
+  var inputVoltage;
+  var outputVoltage;
+  var primaryTurns;
+  var secondaryTurns
+  
+  constructor(i,Np,ivolt,ovolt) {
+    
+  }
 }
